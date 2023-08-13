e14c1f5b1109c5ec0b9d6a8b7231e25f278f2680
Eunice <eunicepatrina0@gmail.com>
2023-08-13 17:54:10 +0530

    migrate the data from text file to database

--- home.py
+++ home.py
@@ -1,9 +1,32 @@
 import streamlit as st
 import plotly.express as px
-import pandas as pd
+import sqlite3
+#import pandas as pd
 
-data = pd.read_csv("data.txt")
+connection = sqlite3.connect("data.db")
 
-fig = px.line(x=data['date'], y=data['temperature'], labels={"x": "Dates", "y": "Temperature(C)"})
-st.plotly_chart(fig)
+def plot_from_textfile():
+    data = pd.read_csv("data.txt")
+    fig = px.line(x=data['date'], y=data['temperature'], labels={"x": "Dates", "y": "Temperature(C)"})
+    st.plotly_chart(fig)
+
+def read():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM temperature")
+    rows = cursor.fetchall()
+    return rows
+
+
+if __name__ == "__main__":
+    values = read()
+    dates = []
+    temperatures = []
+    for value in values:
+        date, temperature = value
+        dates.append(date)
+        temperatures.append(temperature)
+
+    fig = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperature(C)"})
+    st.plotly_chart(fig)
+
 
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import selectorlib
 from datetime import datetime
+import sqlite3
 
 url="http://programmer100.pythonanywhere.com"
-
+connection = sqlite3.connect("data.db")
 def scrapper(url):
     response = requests.get(url)
     source = response.text
@@ -18,8 +19,13 @@ def write(message):
     with open("data.txt", 'a') as file:
         file.write(f'{message}\n')
 
+def write_db(date, temperature):
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperature VALUES(?, ?)",(date, temperature))
+    connection.commit()
+
 if __name__ == "__main__":
     date = datetime.now()
     source = scrapper(url)
     value = extract(source)
-    write(f'{date.strftime("%Y-%m-%d-%H-%M-%S")},{value}')
+    write_db(date.strftime("%Y-%m-%d-%H-%M-%S"), value)
